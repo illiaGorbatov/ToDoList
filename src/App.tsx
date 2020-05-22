a6508987750119bf5e93606fa34a2a39fd187e6a
Harbatau <npikolist@gmail.com>
2020-05-22 20:13:55 +0300

    22052020

--- src/App.tsx
+++ src/App.tsx
@@ -101,23 +101,6 @@ const App = () => {
         trail: 25
     });
 
-    //drag and drop
-    const fn = (order, down, originalIndex, curIndex, y) => index =>
-        down && index === originalIndex
-            ? { y: curIndex * 100 + y, scale: 1.1, zIndex: '1', shadow: 15, immediate: n => n === 'y' || n === 'zIndex' }
-            : { y: order.indexOf(index) * 100, scale: 1, zIndex: '0', shadow: 1, immediate: false }
-
-    function DraggableList({ items }) {
-        const order = useRef(items.map((_, index) => index)) // Store indicies as a local ref, this represents the item order
-        const [springs, setSprings] = useSprings(items.length, fn(order.current)) // Create springs, each corresponds to an item, controlling its transform, scale, etc.
-        const bind = useDrag(({ args: [originalIndex], down, movement: [, y] }) => {
-            const curIndex = order.current.indexOf(originalIndex)
-            const curRow = clamp(Math.round((curIndex * 100 + y) / 100), 0, items.length - 1)
-            const newOrder = swap(order.current, curIndex, curRow)
-            setSprings(fn(newOrder, down, originalIndex, curIndex, y)) // Feed springs new style data, they'll animate the view without causing a single render
-            if (!down) order.current = newOrder
-        }
-    }
 
     console.log(heights)
 
